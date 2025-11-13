7491640a7e34147b856525ac16ff407d7e98b058
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2025-11-13 10:50:33 +0100

    first commit

---
+++ AlienUniverseDatabase/Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+
+namespace AlienUniverseDatabase.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MoviesListBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
