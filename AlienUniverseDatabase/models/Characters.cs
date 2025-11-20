941c5f8548d39b3ed8b8264502f4c210c185f309
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2025-11-20 10:49:52 +0100

    models ful

--- AlienUniverseDatabase/models/Characters.cs
+++ AlienUniverseDatabase/models/Characters.cs
@@ -6,6 +6,13 @@ namespace AlienUniverseDatabase.models
     {
         public string ImieNazwisko { get; set; }
         public string Rola { get; set; }
+        public string Aktor { get; set; }
+        public string Rasa  { get; set; }
+        public string RokUrodzenia { get; set; }
+        public string funkcja { get; set; }
+        public string Charakterystyka { get; set; }
+        public string Los { get; set; }
+        public string Ciekawostka { get; set; }
     }
 
 }
