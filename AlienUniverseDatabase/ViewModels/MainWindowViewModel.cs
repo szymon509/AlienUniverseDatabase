72313e879429b2eb298d0641f88b7b4efee2b65c
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2025-12-04 10:17:15 +0100

    characters

--- AlienUniverseDatabase/ViewModels/MainWindowViewModel.cs
+++ AlienUniverseDatabase/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace AlienUniverseDatabase.ViewModels;
+﻿using System.Collections.ObjectModel;
+using AlienUniverseDatabase.models;
+
+namespace AlienUniverseDatabase.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Alien Universe Database";
+    public ObservableCollection<Movies> Movies { get; } = new()
+    {
+        new Movies()
+        {
+            TytułOryginalny = "Alien", TytułPolski =  "Alien", RokPremiery = 1979, Reżyser = "Ridley Scott", Scenariusz = "Dan O'Bannon", Gatunek = "Sci-Fi / Horror", CzasTrwania = 117, Ocena = 8, GłównePostacie = "Ellen Ripley, Dallas, Ash, Lambert, Kane"},
+    };
 }
